a838efbc660444d0f26738b2fbc6665f7c4dad19
AppShore <git@appshore.com>
2018-12-30 17:04:23 +0000

    refactor fcts in parcels and orders

--- src/orders.js
+++ src/orders.js
@@ -1,5 +1,7 @@
 import { setParcelsUniqId } from './parcels'
-import { sortByDate, sortByNumber } from './sort'
+
+import { sortByDate } from './sort'
+import { parcelsByOrder } from './parcels'
 
 // list all orders
 // ordersSort = true => clockwise
@@ -24,74 +26,6 @@ export const itemsByOrder = (order, items) => {
   }
 }
 
-// compute the parcels per order
-export const parcelsByOrder = order => {
-
-  // sort the order by item weight
-  // true => lighter to heavier
-  // if applied it will change the number of parcels and the potential revenue
-  order.items.sort((a, b) => sortByNumber(a.weight, b.weight, false))
-
-  // split the items into parcels
-  let parcels = order.items.reduce((acc, item) => {
-    let found = false
-
-    // string formatted weight converted to float
-    item.weight = parseFloat(item.weight)
-
-    // map each parcel to find room for a item
-    acc.every(ac => {
-      // check that the weight no more 30
-      let newWeight = ac.weight + item.weight
-      if (newWeight <= 30.0) {
-        // add item to an existing parcel
-        found = true
-        // add weight of item
-        ac.weight = newWeight
-        ac.quantity++
-        // check if same item already exist in the parcel
-        let idx = ac.items.findIndex(a => a.id === item.id)
-        if (idx > -1) {
-          ac.items[idx].quantity++
-        } else {
-          ac.items.push({ ...item, quantity: 1 })
-        }
-        return false
-      }
-      return true
-    })
-
-    if (!found) {
-      // create a new parcel
-      acc.push({
-        order_id: order.id,
-        quantity: 1,
-        weight: parseFloat(item.weight),
-        items: [{ ...item, quantity: 1 }]
-      })
-    }
-    return acc
-  }, [])
-
-  const priceWeight = [
-    { min: 0.0, max: 1.0, price: 1 },
-    { min: 1.0, max: 5.0, price: 2 },
-    { min: 5.0, max: 10.0, price: 3 },
-    { min: 10.0, max: 20.0, price: 5 },
-    { min: 20.0, max: 30.0, price: 10 }
-  ]
-
-  parcels.map(p => {
-    // determine revenue by parcel according priceWeight
-    let pw = priceWeight.find(pw => pw.min < p.weight && p.weight <= pw.max)
-    p.revenue = pw.price
-    // format weight with 2 decimals
-    p.weight = parseFloat(p.weight.toFixed(2))
-  })
-
-  return parcels
-}
-
 // Weight by order
 // work with items or parcels
 export const weightByOrder = entities =>
@@ -102,11 +36,8 @@ export const weightByOrder = entities =>
 export const revenueByOrder = parcels =>
   parcels.reduce((acc, parcel) => acc + parseFloat(parcel.revenue), 0.0)
 
-// main fct that process all
-export const processOrders = async (orders, items) => {
-  // map the orders array, async is mandatory to handle
-  // the fetch of uniqids from server
-  let promises = orders.map(async order => {
+// process one order
+export const processOrder = async (order, items) => {
     // retrieve each unique item
     let itemsOrder = itemsByOrder(order, items)
 
@@ -152,7 +83,10 @@ export const processOrders = async (orders, items) => {
       weight: parseFloat(weight.toFixed(2)),
       parcels: parcelsOrder
     }
-  })
+}
 
-  return await Promise.all(promises)
+// main fct that process all orders
+export const processOrders = async (orders, items) => {
+  // map the orders array which returns an array of promises 
+  return await Promise.all(orders.map(async order => processOrder(order, items)))
 }
